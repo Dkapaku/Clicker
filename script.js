be38ecedf6dc50566c1e9ce817edeb704be066f9
David Kapaku <dkapaku1@vt.edu>
2023-02-03 16:04:06 -0500

    Inititialized Project

---
+++ script.js
@@ -0,0 +1,19 @@
+//Click and Picture
+let pictureElement = document.getElementById("Picture")
+let countElement = document.getElementById("Count")
+console.log(countElement)
+let clickerCount = 0
+function changecountNumber(){
+    console.log("+3");
+    clickerCount = clickerCount +3;
+    countElement.textContent = clickerCount;
+}
+pictureElement.addEventListener("click", changecountNumber)
+
+let ionianBootCost = 10
+let ionianBootCount = 0;
+//Eventlistener, (make a button for each item), add an event listener to button
+// when the button is clicked ??? check the count vs cost, in enough, decrease total, change price, 
+//keep track of how many of item owned/ how much its going up
+let item1Element = document.getElementById("item1cost")
+let 
